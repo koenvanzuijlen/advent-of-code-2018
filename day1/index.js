cdbc8c585d3b3f71ad2aacffc479a0be0a520a91
koenvanzuijlen <khavanzuijlen@gmail.com>
2018-12-02 15:40:08 +0100

    Add solution to day one part 2

--- day1/index.js
+++ day1/index.js
@@ -1,19 +1,41 @@
 const chalk = require("chalk");
 const fs = require("fs");
 
-module.exports = () => {
-	let frequency = 0;
-
+module.exports = (half = 1) => {
 	const input = fs
 		.readFileSync("./day1/input", { encoding: "utf-8" })
 		.split("\n");
 
-	input.forEach(frequencyChange => {
-		frequencyChange = parseInt(frequencyChange);
-		if (!isNaN(frequencyChange)) {
-			frequency += frequencyChange;
-		}
-	});
+	if (half === 1) {
+		let frequency = 0;
 
-	console.log(chalk.blue(`Frequency: ${frequency}`));
+		input.forEach(frequencyChange => {
+			frequencyChange = parseInt(frequencyChange);
+			if (!isNaN(frequencyChange)) {
+				frequency += frequencyChange;
+			}
+		});
+
+		return console.log(chalk.blue(`Frequency: ${frequency}`));
+	
+	} else if (half === 2) {
+		let frequency = 0;
+		const frequenciesSeen = [];
+
+		while(true) {
+			for(let frequencyChange of input) {
+				frequencyChange = parseInt(frequencyChange);
+				if (!isNaN(frequencyChange)) {
+					frequency += frequencyChange;
+					if(frequenciesSeen.indexOf(frequency) === -1) {
+						//Not seen yet
+						frequenciesSeen.push(frequency);						
+					} else {
+						//Found the first repeating frequency
+						return console.log(chalk.blue(`First repeated frequency: ${frequency}`));
+					}
+				}
+			};
+		}
+	}
 };
--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const fs = require("fs");
 console.log(chalk.bold.magenta("Advent of Code 2018 by koenvanzuijlen"));
 
 if (!process.argv[2]) {
-	return console.log(chalk.red("Please enter the day you wish to run"));
+	return console.log(
+		chalk.red("Please enter the day and half you wish to run, for example 3-1"),
+	);
 }
 
-const day = parseInt(process.argv[2]);
+const argParts = process.argv[2].split("-");
+const day = parseInt(argParts[0]);
+const half = parseInt(argParts[1]);
+if (isNaN(day) || [1, 2].indexOf(half) === -1) {
+	return console.log(chalk.red("Invalid argument"));
+}
 if (!fs.existsSync(`./day${day}/index.js`)) {
-	return console.log(chalk.red("Invalid day"));
+	return console.log(chalk.red("This half and/or day aren't available (yet)"));
 }
 
-console.log(chalk.green(`Solving day ${day}`));
+console.log(chalk.green(`Solving half ${half} of day ${day}`));
 const dayScript = require(`./day${day}`);
-dayScript();
+dayScript(half);
